fe32f868b7bc9fe0e347a43f8d53a1fb3a989205
Xetha666 <bryans27092000@gmail.com>
2026-04-28 11:48:50 -0500

    Add initial App component with custom text and styling

---
+++ App.tsx
@@ -0,0 +1,30 @@
+import CustomText from '@/components/CustomText';
+import "@/styles/global.css";
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <CustomText>¡Hola desde un componente con alias!</CustomText>
+      <StatusBar style="auto" />
+
+      <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-xl font-bold text-blue-500">
+        Welcome to Nativewind!
+      </Text>
+    </View>
+    </View>
+
+    
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
